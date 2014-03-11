8db380a7d71787b9d8f2eeca5551fbecab9bd4ea
ShaoKai <dylan.yang@dylanyang-nb.tw.smi.ad>
2014-03-11 18:44:40 +0800

    add source folder.

---
+++ src/main/java/com/sky/mahout/App.java
@@ -0,0 +1,21 @@
+package com.sky.mahout;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+public class App {
+
+	public static void main(String[] args) throws Exception {
+
+	}
+}
